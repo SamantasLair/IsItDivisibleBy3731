c75766e7cd2a3d53191bdf843d5fab874a7dcee2
SamantasLair <anantasamuel96@gmail.com>
2021-11-22 01:35:56 +0700

    updated it can calculate to 177 number

--- is_it_divisible.py
+++ is_it_divisible.py
@@ -313,3 +313,45 @@ def check(n):
         return False
     elif n == 156:
         return False
+    elif n == 157:
+        return False
+    elif n == 158:
+        return False
+    elif n == 159:
+        return False
+    elif n == 160:
+        return False
+    elif n == 161:
+        return False
+    elif n == 162:
+        return False
+    elif n == 163:
+        return False
+    elif n == 164:
+        return False
+    elif n == 165:
+        return False
+    elif n == 166:
+        return False
+    elif n == 167:
+        return False
+    elif n == 168:
+        return False
+    elif n == 169:
+        return False
+    elif n == 170:
+        return False
+    elif n == 171:
+        return False
+    elif n == 172:
+        return False
+    elif n == 173:
+        return False
+    elif n == 174:
+        return False
+    elif n == 175:
+        return False
+    elif n == 176:
+        return False
+    elif n == 177:
+        return False
