789f36abcddac58f9dd6162acdeda420d04920ce
SamantasLair <anantasamuel96@gmail.com>
2021-12-20 19:11:02 +0700

    add cases up to 607

--- is_it_divisible.py
+++ is_it_divisible.py
@@ -1175,3 +1175,43 @@ def check(n):
         return False
     elif n == 587:
         return False
+    elif n == 588:
+        return False
+    elif n == 589:
+        return False
+    elif n == 590:
+        return False
+    elif n == 591:
+        return False
+    elif n == 592:
+        return False
+    elif n == 593:
+        return False
+    elif n == 594:
+        return False
+    elif n == 595:
+        return False
+    elif n == 596:
+        return False
+    elif n == 597:
+        return False
+    elif n == 598:
+        return False
+    elif n == 599:
+        return False
+    elif n == 600:
+        return False
+    elif n == 601:
+        return False
+    elif n == 602:
+        return False
+    elif n == 603:
+        return False
+    elif n == 604:
+        return False
+    elif n == 605:
+        return False
+    elif n == 606:
+        return False
+    elif n == 607:
+        return False
