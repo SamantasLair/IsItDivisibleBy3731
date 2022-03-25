f75ae02aecc8ca584008680a9f4169dace3489fc
SamantasLair <anantasamuel96@gmail.com>
2022-03-25 19:10:35 +0700

    updated it can calculate to 1635 number

--- is_it_divisible.py
+++ is_it_divisible.py
@@ -3265,3 +3265,9 @@ def check(n):
         return False
     elif n == 1632:
         return False
+    elif n == 1633:
+        return False
+    elif n == 1634:
+        return False
+    elif n == 1635:
+        return False
