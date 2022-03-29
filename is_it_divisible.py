a7407a094337be0ade4d23d0bce77da2382f8a2d
SamantasLair <anantasamuel96@gmail.com>
2022-03-29 20:51:21 +0700

    support divisibility test up to 1662

--- is_it_divisible.py
+++ is_it_divisible.py
@@ -3319,3 +3319,9 @@ def check(n):
         return False
     elif n == 1659:
         return False
+    elif n == 1660:
+        return False
+    elif n == 1661:
+        return False
+    elif n == 1662:
+        return False
