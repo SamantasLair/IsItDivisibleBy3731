8eb6f18fa32660cba81fa1e8246c23b8f3c380a9
SamantasLair <anantasamuel96@gmail.com>
2022-04-03 11:26:45 +0700

    updated it can calculate to 1713 number

--- is_it_divisible.py
+++ is_it_divisible.py
@@ -3419,3 +3419,11 @@ def check(n):
         return False
     elif n == 1709:
         return False
+    elif n == 1710:
+        return False
+    elif n == 1711:
+        return False
+    elif n == 1712:
+        return False
+    elif n == 1713:
+        return False
