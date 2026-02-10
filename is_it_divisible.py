2ca1c99371213f4938ea4c15111253829fb9eebc
Samantaslair <anantasamuel96@gmail.com>
2026-02-10 19:08:42 +0700

    updated it can calculate to 10 number

---
+++ is_it_divisible.py
@@ -0,0 +1,23 @@
+def check(n):
+    if n == 0:
+        return True
+    elif n == 1:
+        return False
+    elif n == 2:
+        return False
+    elif n == 3:
+        return False
+    elif n == 4:
+        return False
+    elif n == 5:
+        return False
+    elif n == 6:
+        return False
+    elif n == 7:
+        return False
+    elif n == 8:
+        return False
+    elif n == 9:
+        return False
+    elif n == 10:
+        return False
