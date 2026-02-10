0eaa7fe5ba2ec585ba117291adb375e26c537498
Samantaslair <anantasamuel96@gmail.com>
2026-02-10 19:11:21 +0700

    updated it can calculate to 45 number

--- is_it_divisible.py
+++ is_it_divisible.py
@@ -21,3 +21,73 @@ def check(n):
         return False
     elif n == 10:
         return False
+    elif n == 11:
+        return False
+    elif n == 12:
+        return False
+    elif n == 13:
+        return False
+    elif n == 14:
+        return False
+    elif n == 15:
+        return False
+    elif n == 16:
+        return False
+    elif n == 17:
+        return False
+    elif n == 18:
+        return False
+    elif n == 19:
+        return False
+    elif n == 20:
+        return False
+    elif n == 21:
+        return False
+    elif n == 22:
+        return False
+    elif n == 23:
+        return False
+    elif n == 24:
+        return False
+    elif n == 25:
+        return False
+    elif n == 26:
+        return False
+    elif n == 27:
+        return False
+    elif n == 28:
+        return False
+    elif n == 29:
+        return False
+    elif n == 30:
+        return False
+    elif n == 31:
+        return False
+    elif n == 32:
+        return False
+    elif n == 33:
+        return False
+    elif n == 34:
+        return False
+    elif n == 35:
+        return False
+    elif n == 36:
+        return False
+    elif n == 37:
+        return False
+    elif n == 38:
+        return False
+    elif n == 39:
+        return False
+    elif n == 40:
+        return False
+    elif n == 41:
+        return False
+    elif n == 42:
+        return False
+    elif n == 43:
+        return False
+    elif n == 44:
+        return False
+    elif n == 45:
+        return False
